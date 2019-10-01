14974c1893294dd9374297747be415cbd1235b02
ToddMG <ToddMG@users.noreply.github.com>
2019-10-01 14:20:35 -0700

    Added class for DataFrame Management

--- lambdata_ToddMG/df_utils.py
+++ lambdata_ToddMG/df_utils.py
@@ -3,3 +3,24 @@ utility functions for working with DataFrames
 """
 
 import pandas
+
+class DFManager():
+    """
+    Helper class for dataframe functions/methods
+    """
+
+    def __intit__(self, DF):
+        self.df = df
+
+    def mergeRows(self, df1, df2):
+        df_new = pd.concat([df1, df2])
+        return df_new
+
+    def mergeCols(self, df1, df2):
+        df_new = pd.concat([df1, df2, axis=1])
+        return df_new
+
+    def newCol(self, df, name, list):
+        df[name] = list
+        return df
+
