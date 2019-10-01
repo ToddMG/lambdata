2e6a6277fb5c9fe5b24fbe7e4d312766aee0a0d5
ToddMG <ToddMG@users.noreply.github.com>
2019-10-01 14:26:53 -0700

    Updated method documentation

--- lambdata_ToddMG/df_utils.py
+++ lambdata_ToddMG/df_utils.py
@@ -13,14 +13,33 @@ class DFManager():
         self.df = df
 
     def mergeRows(self, df1, df2):
+        """
+        A function to merge two data frames by rows
+        :param df1: Dataframe to merge with
+        :param df2: Dataframe to merge
+        :return: Merged dataframe
+        """
         df_new = pd.concat([df1, df2])
         return df_new
 
     def mergeCols(self, df1, df2):
+        """
+        A function to merge two data frames by columns
+        :param df1: Dataframe to merge with
+        :param df2: Dataframe to merge
+        :return: Merged dataframe
+        """
         df_new = pd.concat([df1, df2, axis=1])
         return df_new
 
     def newCol(self, df, name, list):
+        """
+        A function to create a new column for a dataframe
+        :param df: Dataframe
+        :param name: Name of column
+        :param list: List or series to create column
+        :return: New Dataframe
+        """
         df[name] = list
         return df
 
